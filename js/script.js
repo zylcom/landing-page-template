1181a41f06e0a61bcb764232b2bb852fc7a770f4
sabilillah <sabilillah272@gmail.com>
2021-10-14 19:25:42 +0700

    fix class name, id element and, nav link

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-const banner = [...document.querySelectorAll("#banner-wrapper img")];
+// const banner = [...document.querySelectorAll("#banner-wrapper img")];
 
-let index = 0;
-const changeCarousel = () => {
-  banner[index].classList.toggle("active");
+// let index = 0;
+// const changeCarousel = () => {
+//   banner[index].classList.toggle("active");
 
-  if (index >= banner.length - 1) {
-    index = 0;
-  } else {
-    index++;
-  }
-  banner[index].classList.toggle("active");
-};
+//   if (index >= banner.length - 1) {
+//     index = 0;
+//   } else {
+//     index++;
+//   }
+//   banner[index].classList.toggle("active");
+// };
 
-setInterval(() => {
-  changeCarousel();
-}, 5000);
+// setInterval(() => {
+//   changeCarousel();
+// }, 5000);
 
 function scrollUp() {
   const scrollUp = document.querySelector(".scroll-up");

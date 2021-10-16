80d2d9ca37af242126d96acef5e0aecf4d26d2bf
sabilillah <sabilillah272@gmail.com>
2021-10-16 10:35:30 +0700

    add style to active nav link

--- js/script.js
+++ js/script.js
@@ -16,6 +16,17 @@
 //   changeCarousel();
 // }, 5000);
 
+const navLink = document.getElementsByClassName("nav-link");
+
+for (const link of navLink) {
+  link.addEventListener("click", function () {
+    for (const item of navLink) {
+      item.classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+}
+
 function scrollUp() {
   const scrollUp = document.querySelector(".scroll-up");
   if (this.scrollY >= 200) scrollUp.classList.add("show");
